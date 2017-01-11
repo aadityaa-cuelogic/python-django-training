eb549f09af3f5b18869e5a4e6377604e5e1388ff
Aaditya Agrawal <aaditya.agrawal@cuelogic.com>
2017-01-11 20:24:25 +0530

    added datetime in model

--- firstprojectsite/polls/models.py
+++ firstprojectsite/polls/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
+import datetime
 from django.utils import timezone
-
 from django.db import models
 
 # Create your models here.
